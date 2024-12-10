d8ed77c17b48bfedd783ad1f3174429c8a0142d8
INS <89240832+ins20@users.noreply.github.com>
2024-12-10 20:53:47 +0400

    Update list.tsx

--- src/pages/statistics/list.tsx
+++ src/pages/statistics/list.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, List } from "@mantine/core";
+import { Box, Button, List, Text, Group } from "@mantine/core";
 import { useList } from "@refinedev/core";
 import { Patient } from "../../types";
 import { IconDownload } from "@tabler/icons-react";
@@ -10,6 +10,7 @@ import {
   useMantineReactTable,
 } from "mantine-react-table";
 import React from "react";
+import { BarChart, LineChart, PieChart } from "@mantine/charts";
 
 // @ts-ignore
 import { mkConfig, generateCsv, download } from "export-to-csv"; //or use your library of choice here
@@ -19,6 +20,10 @@ const csvConfig = mkConfig({
   decimalSeparator: ".",
   useKeysAsHeaders: true,
 });
+
+function getFullYear(date: Date | string) {
+  return new Date().getFullYear() - new Date(date).getFullYear();
+}
 
 const Statistics = () => {
   const patientsByTherapist = useList<Patient>({
@@ -155,6 +160,197 @@ const Statistics = () => {
   }, [sorting]);
   return (
     <List>
+      <Group gap={50} mb={10} display={'flex'}>
+        <div>
+          <Text fz="xs" mb="sm" ta="center">
+            Болезнь
+          </Text>
+          <PieChart
+            withTooltip
+            tooltipDataSource="segment"
+            withLabelsLine
+            labelsPosition="outside"
+            labelsType="value"
+            withLabels
+            data={[
+              {
+                name: "БП",
+                value: data.filter((e) => e.bp).length,
+                color: "indigo.6",
+              },
+              {
+                name: "ДЭП",
+                value: data.filter((e) => e.dep).length,
+                color: "yellow.6",
+              },
+              {
+                name: "Ишемия",
+                value: data.filter((e) => e.ischemia).length,
+                color: "teal.6",
+              },
+            ]}
+          />
+        </div>
+        <div>
+          <Text fz="xs" mb="sm" ta="center">
+            Пол
+          </Text>
+          <PieChart
+            withTooltip
+            tooltipDataSource="segment"
+            withLabelsLine
+            labelsPosition="outside"
+            labelsType="value"
+            withLabels
+            data={[
+              {
+                name: "Мужской",
+                value: data.filter((e) => e.gender === "м").length,
+                color: "indigo.6",
+              },
+              {
+                name: "Женский",
+                value: data.filter((e) => e.gender === "ж").length,
+                color: "yellow.6",
+              },
+            ]}
+          />
+        </div>
+        <div>
+          <Text fz="xs" mb="sm" ta="center">
+            Населенный пункт
+          </Text>
+          <PieChart
+            withTooltip
+            tooltipDataSource="segment"
+            withLabelsLine
+            labelsPosition="outside"
+            labelsType="value"
+            withLabels
+            data={[
+              {
+                name: "Город",
+                value: data.filter((e) => e.inhabited_locality === "Город")
+                  .length,
+                color: "indigo.6",
+              },
+              {
+                name: "Район",
+                value: data.filter((e) => e.inhabited_locality === "Район")
+                  .length,
+                color: "yellow.6",
+              },
+            ]}
+          />
+        </div>
+        <div>
+          <Text fz="xs" mb="sm" ta="center">
+            Болезнь по полу
+          </Text>
+          <BarChart
+            w={300}
+            h={300}
+            data={[
+              {
+                gender: "М",
+                БП: data.filter((e) => e.bp && e.gender === "м").length,
+                Ишемия: data.filter((e) => e.ischemia && e.gender === "м")
+                  .length,
+                ДЭП: data.filter((e) => e.dep && e.gender === "м").length,
+              },
+              {
+                gender: "Ж",
+                БП: data.filter((e) => e.bp && e.gender === "ж").length,
+                Ишемия: data.filter((e) => e.ischemia && e.gender === "ж")
+                  .length,
+                ДЭП: data.filter((e) => e.dep && e.gender === "ж").length,
+              },
+            ]}
+            dataKey="gender"
+            type="stacked"
+            series={[
+              { name: "БП", color: "violet.6" },
+              { name: "Ишемия", color: "blue.6" },
+              { name: "ДЭП", color: "teal.6" },
+            ]}
+          />
+        </div>
+        <div>
+          <Text fz="xs" mb="sm" ta="center">
+            Населенный пункт по полу
+          </Text>
+          <BarChart
+            w={300}
+            h={300}
+            data={[
+              {
+                inhabited_locality: "Город",
+                БП: data.filter((e) => e.bp && e.inhabited_locality === "Город")
+                  .length,
+                Ишемия: data.filter(
+                  (e) => e.ischemia && e.inhabited_locality === "Город"
+                ).length,
+                ДЭП: data.filter(
+                  (e) => e.dep && e.inhabited_locality === "Город"
+                ).length,
+              },
+              {
+                inhabited_locality: "Район",
+                БП: data.filter((e) => e.bp && e.inhabited_locality === "Район")
+                  .length,
+                Ишемия: data.filter(
+                  (e) => e.ischemia && e.inhabited_locality === "Район"
+                ).length,
+                ДЭП: data.filter(
+                  (e) => e.dep && e.inhabited_locality === "Район"
+                ).length,
+              },
+            ]}
+            dataKey="inhabited_locality"
+            type="stacked"
+            series={[
+              { name: "БП", color: "violet.6" },
+              { name: "Ишемия", color: "blue.6" },
+              { name: "ДЭП", color: "teal.6" },
+            ]}
+          />
+        </div>
+        <div>
+          <Text fz="xs" mb="sm" ta="center">
+            Болезнь по возрастам
+          </Text>
+          <LineChart
+            w={1000}
+            h={300}
+            data={Array.from(
+              new Set(data.map((e) => getFullYear(e.birthday || "")))
+            )
+              .sort((a, b) => a - b)
+              .map((age) => {
+                return {
+                  age,
+                  БП: data.filter(
+                    (e) => getFullYear(e.birthday || "") === age && e.bp
+                  ).length,
+                  ДЭП: data.filter(
+                    (e) => getFullYear(e.birthday || "") === age && e.dep
+                  ).length,
+                  Ишемия: data.filter(
+                    (e) => getFullYear(e.birthday || "") === age && e.ischemia
+                  ).length,
+                };
+              })}
+            dataKey="age"
+            series={[
+              { name: "БП", color: "indigo.6" },
+              { name: "ДЭП", color: "blue.6" },
+              { name: "Ишемия", color: "teal.6" },
+            ]}
+            curveType="linear"
+            tickLine="xy"
+          />
+        </div>
+      </Group>
       <MantineReactTable table={table} />
     </List>
   );
